1749de141020e04739ecfab80c72f402d6cc8b0a
GrampaSheev <94834543+GrampaSheev@users.noreply.github.com>
2022-06-30 10:57:50 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,5 +1,6 @@
 cd /D %HOMEDRIVE%\
 cd Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+del BlockTheSpot.bat
 (
 echo @echo off
 echo powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/GrampaSheev/BlockTheSpotAuto/master/install.ps1' | Invoke-Expression}"
