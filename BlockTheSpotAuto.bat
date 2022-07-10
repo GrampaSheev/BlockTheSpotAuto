642fdc280e5f91be3f9c14ccb3bbeed4c8a6ff0f
GrampaSheev <94834543+GrampaSheev@users.noreply.github.com>
2022-07-10 11:42:59 +0100

    Update BlockTheSpotAuto.bat

--- BlockTheSpotAuto.bat
+++ BlockTheSpotAuto.bat
@@ -1,4 +1,3 @@
 @echo off
 powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/GrampaSheev/BlockTheSpotAuto/master/install.ps1' | Invoke-Expression}"
-pause
 exit
